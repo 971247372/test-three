c496f47cdcb29390232f9d937f8bf12f32bf560e
qingwan_zeng <qingwan_zeng@fengzhiding.com>
2019-06-05 17:56:59 +0800

    加入初始平面

--- src/App.js
+++ src/App.js
@@ -1,40 +1,41 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-const THREE = require('three')
-const OrbitControls = require('three-orbitcontrols')
+const THREE = require("three");
+const OrbitControls = require("three-orbitcontrols");
 class App extends Component {
   componentDidMount() {
-  const width = window.innerWidth
-const height = window.innerHeight
+    const width = window.innerWidth;
+    const height = window.innerHeight;
 
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, width/height, 0.1, 1000)
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+    camera.position.set(0, 10, 0);
+    camera.lookAt(0, 0, 0);
+    camera.position.z = 5;
+    const paneGeometry = new THREE.PlaneGeometry(10, 10, 10);
+    const paneMaterial = new THREE.MeshNormalMaterial({
+      color: 0x00ff00,
+      side: THREE.DoubleSide,
+      wireframe: true
+    });
+    const pane = new THREE.Mesh(paneGeometry, paneMaterial);
+    pane.rotateX(Math.PI * 0.25);
 
+    scene.add(pane);
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(width, height)
-document.body.appendChild(renderer.domElement)
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(width, height);
+    document.body.appendChild(renderer.domElement);
+    const controls = new OrbitControls(camera, renderer.domElement);
+    const animate = () => {
+      requestAnimationFrame(animate);
+      renderer.render(scene, camera);
+    };
 
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
-
-camera.position.z = 5
-
-const controls = new OrbitControls(camera, renderer.domElement)
-
-const animate=()=> {
-  requestAnimationFrame(animate)
-
-  renderer.render(scene, camera)
-}
-
-animate()
-}
+    animate();
+  }
 
   render() {
-   
     return <div />;
   }
 }
